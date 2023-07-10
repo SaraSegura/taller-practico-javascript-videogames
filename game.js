ad2204d3e0539082b254a3eb05ef5d85afb700c2
sara segura <sarasegura224@gmail.com>
2023-07-10 17:11:02 -0500

    [feat] Se dectecta colisión con el enemigo.

--- game.js
+++ game.js
@@ -18,6 +18,8 @@ const giftPosition = {
   y: undefined,
 };
 
+let enemyPositions = [];
+
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
 
@@ -49,6 +51,8 @@ function startGame() {
   const mapRowCols = mapRows.map((row) => row.trim().split(""));
   console.log({ map, mapRows, mapRowCols });
 
+  enemyPositions = [];
+
   //vamos a recorrer este array bidimencional
   // forEach() ejecuta la función indicada una vez por cada elemento del arreglo.
   game.clearRect(0, 0, canvasSize, canvasSize);
@@ -69,6 +73,11 @@ function startGame() {
       } else if (col == "I") {
         giftPosition.x = posX;
         giftPosition.y = posY;
+      } else if (col == "X") {
+        enemyPositions.push({
+          x: posX,
+          y: posY,
+        });
       }
 
       game.fillText(emoji, posX, posY);
@@ -87,6 +96,16 @@ function movePlayer() {
 
   if (giftCollision) {
     console.log("Subiste de nisvel!");
+  }
+
+  const enemyCollision = enemyPositions.find((enemy) => {
+    const enemyCollisionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3);
+    const enemyCollisionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3);
+    return enemyCollisionX && enemyCollisionY;
+  });
+
+  if (enemyCollision) {
+    console.log("Chocaste con un enemigo");
   }
 
   game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
