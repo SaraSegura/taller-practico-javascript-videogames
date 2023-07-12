0b5655a2f75491a5f25dfc587a48d0412c91f98f
sara segura <sarasegura224@gmail.com>
2023-07-12 09:56:02 -0500

    [feat] Se agrega el record.

--- game.js
+++ game.js
@@ -6,6 +6,8 @@ const btnRight = document.querySelector("#right");
 const btnDown = document.querySelector("#down");
 const spanLives = document.querySelector("#lives");
 const spanTime = document.querySelector("#time");
+const spanRecord = document.querySelector("#record");
+const pResult = document.querySelector("#result");
 
 let canvasSize;
 let elementsSize;
@@ -62,6 +64,7 @@ function startGame() {
   if (!timeStart) {
     timeStart = Date.now();
     timeInterval = setInterval(showTime, 100);
+    showRecord();
   }
   // variable: filas del mapa,metodo .trim: limpiar el estrin, quitar los espacios. metodo .split : creando un arreglo a partir de un estrin. ("\n") cada vez que alla un salto de linea
   const mapRows = map.trim().split("\n");
@@ -161,6 +164,21 @@ function levelFail() {
 function gameWin() {
   console.log("terminas el juego");
   clearInterval(timeInterval);
+
+  const recordTime = localStorage.getItem("record_time");
+  const playerTime = Date.now() - timeStart;
+  if (recordTime) {
+    if (recordTime >= playerTime) {
+      localStorage.setItem("record_time", playerTime);
+      pResult.innerHTML = "Superaste el record";
+    } else {
+      pResult.innerHTML = "No se supero el record";
+    }
+  } else {
+    localStorage.setItem("record_time", playerTime);
+    pResult.innerHTML = "Crea un record";
+  }
+  console.log({ recordTime, playerTime });
 }
 
 function showLives() {
@@ -171,6 +189,10 @@ function showLives() {
 }
 function showTime() {
   spanTime.innerHTML = Date.now() - timeStart;
+}
+
+function showRecord() {
+  spanRecord.innerHTML = localStorage.getItem("record_time");
 }
 
 function moveByKeys(event) {
